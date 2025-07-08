45ca9cb52680f41bdb4d50690baa07cd67fcbf4f
Rim Jouili <160032853+RIM670@users.noreply.github.com>
2025-07-08 13:32:55 +0200

    Update deploy-prod.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,23 +3,17 @@ set -e
 
 echo "🚀 Déploiement des modules sur Odoo Prod..."
 
-# Variables à adapter
+
 USER_PROD=rim
-HOST_PROD=192.168.17.130  # IP de ta VM Odoo installée manuellement
+HOST_PROD=192.168.17.130  
 DEST_PATH=/opt/odoo/custom_addons/
 
-# Copier les modules personnalisés
-#echo "📦 Copie des modules personnalisés vers la VM prod..."
-#scp -r custom_addons/* $USER_PROD@$HOST_PROD:$DEST_PATH
 
-# Copier le fichier de config si nécessaire (facultatif)
-# scp config/odoo.conf $USER_PROD@$HOST_PROD:/etc/odoo/odoo.conf
 
-# Redémarrer le service Odoo
-echo "♻️ Redémarrage du service Odoo sur la VM prod..."
+echo " Redémarrage du service Odoo sur la VM prod..."
 ssh $USER_PROD@$HOST_PROD "sudo systemctl restart odoo17"
 
-echo "✅ Déploiement terminé avec succès."
+echo "Déploiement terminé avec succès."
 ssh $USER_PROD@$HOST_PROD "echo '?? Service odoo17 status :' && systemctl status odoo17 | head -n 10"
 ssh $USER_PROD@$HOST_PROD "ps -eo pid,lstart,cmd | grep odoo | grep -v grep"
 
